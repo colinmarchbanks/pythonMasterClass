e4448d0ee03337a65f96efc904fdc81ab4611e1a
colinmarchbanks <marchbanks.colin@gmail.com>
2021-02-18 20:41:23 -0600

    Wrapping up today, finished section 3.17, 18/2/2021

---
+++ hello-world/strings.py
@@ -0,0 +1,11 @@
+print("Today is a good day to learn python")
+print('python is fun')
+print("Python's strings are easy to use")
+print('You can even use quotations in python! ("")')
+print("hello" + " world")
+greeting = "Hello"
+name = input("Please enter a name ")
+
+#comments comments comments
+
+print(greeting + ' ' + name)
