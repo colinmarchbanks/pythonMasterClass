972827aa6ec9bd83cb538629bcae46896a49987e
colinmarchbanks <marchbanks.colin@gmail.com>
2021-02-19 16:53:02 -0600

    Finished section 3.24

--- hello-world/strings.py
+++ hello-world/strings.py
@@ -19,3 +19,14 @@ print("age:",age)
 print("type(age):",type(age))
 
 # Python doesn't set the value of a variable, but rather rebinds the value of the variable to the new value that has been set equal to, no more binding functions
+
+#Python 3 ints dont have a max value, so the computer will run out of memorary first :)))
+
+a = 12
+b = 3
+print(a / b) #4.0 regular division
+print(a // b) #4 interger division rounded down
+print(a % b) #0 modulo divisio, remainder after the interger division
+
+for i in range(1,a//b):
+    print(i)
